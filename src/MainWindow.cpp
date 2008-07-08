106c79d59c2a895d969631e9072064eaf5102ae5
Lubos Dolezel <lubos@dolezel.info>
2008-07-08 19:38:52 +0200

    Added a missing #include

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -29,6 +29,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <QDropEvent>
 #include <QtDebug>
 #include <QProcess>
+#include <QSettings>
 #include <QUrl>
 
 #include "MainWindow.h"
