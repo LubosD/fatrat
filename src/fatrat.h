74c58255698bfda4d95340fdb06a09482ea94c89
Lubos Dolezel <lubos@dolezel.info>
2008-07-15 16:45:19 +0200

    Fixing Git errors #1

--- src/fatrat.h
+++ src/fatrat.h
@@ -30,7 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <QIcon>
 #include <QNetworkProxy>
 
-#define VERSION "DEV"
+#define VERSION "DEV" 
 
 QString formatSize(qulonglong size, bool persec = false);
 QString formatTime(qulonglong secs);
