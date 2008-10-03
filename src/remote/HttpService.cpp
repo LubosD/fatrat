aadcb452999b766433d6a9ba74ee6f8efd8e3d7b
Lubos Dolezel <lubos@dolezel.info>
2008-10-04 00:19:40 +0200

    Removed an epoll #include from HttpService

--- src/remote/HttpService.cpp
+++ src/remote/HttpService.cpp
@@ -47,7 +47,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/epoll.h>
 #include <netinet/in.h>
 #include <cstdlib>
 #include <locale.h>
