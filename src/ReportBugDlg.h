5b6ec1d978b973cda7547f09d238d57541ae59f0
Lubos Dolezel <lubos@dolezel.info>
2009-04-06 14:59:16 +0200

    Added an OpenSSL exception into all source file headers

--- src/ReportBugDlg.h
+++ src/ReportBugDlg.h
@@ -6,7 +6,8 @@ Copyright (C) 2006-2009 Lubos Dolezel <lubos a dolezel.info>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-version 2 as published by the Free Software Foundation.
+version 2 as published by the Free Software Foundation
+with the OpenSSL special exemption.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
