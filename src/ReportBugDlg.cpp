9a369401d586fb61a9c5efeaece7485dc06b8f67
Lubos Dolezel <lubos@dolezel.info>
2009-04-07 09:44:06 +0200

    Revert "Added an OpenSSL exception into all source file headers"
    This reverts commit 5b6ec1d978b973cda7547f09d238d57541ae59f0.

--- src/ReportBugDlg.cpp
+++ src/ReportBugDlg.cpp
@@ -6,8 +6,7 @@ Copyright (C) 2006-2009 Lubos Dolezel <lubos a dolezel.info>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-version 2 as published by the Free Software Foundation
-with the OpenSSL special exemption.
+version 2 as published by the Free Software Foundation.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
