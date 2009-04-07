8ceed7a0a748ce422ce2e68489d1f849cfcb8ac9
Lubos Dolezel <lubos@dolezel.info>
2009-04-07 10:19:16 +0200

    Added a full OpenSSL exception into all source file headers

--- src/SettingsDropBoxForm.h
+++ src/SettingsDropBoxForm.h
@@ -16,6 +16,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+In addition, as a special exemption, Luboš Doležel gives permission
+to link the code of FatRat with the OpenSSL project's
+"OpenSSL" library (or with modified versions of it that use the; same
+license as the "OpenSSL" library), and distribute the linked
+executables. You must obey the GNU General Public License in all
+respects for all of the code used other than "OpenSSL".
 */
 
 #ifndef SETTINGSDROPBOXFORM_H
