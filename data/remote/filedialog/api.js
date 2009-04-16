8b47c973d8c3fba2b79645e70968317e9157724d
Lubos Dolezel <lubos@dolezel.info>
2009-04-16 17:57:40 +0200

    Added scrollbars into the dir dialog in the web interface

--- data/remote/filedialog/api.js
+++ data/remote/filedialog/api.js
@@ -4,7 +4,7 @@ var lastDirElem;
 function dirDialog(elemId)
 {
 	var elem = document.getElementById(elemId);
-	var win = window.open('/filedialog/dirdialog.html?'+elem.value, 'dirdialog', 'width=660,height=500,toolbar=0');
+	var win = window.open('/filedialog/dirdialog.html?'+elem.value, 'dirdialog', 'width=660,height=500,toolbar=0,scrollbars=1');
 	lastDirElem = elem;
 }
 
