60917e2070cc75d0c66586e76193a8441814d14e
Lubos Dolezel <lubos@dolezel.info>
2011-01-29 00:14:38 +0100

    Fix CreateSymlinks.cmake support for DESTDIR

--- CreateSymlinks.cmake
+++ CreateSymlinks.cmake
@@ -1,6 +1,1 @@
-if (DESTDIR)
-	EXECUTE_PROCESS(COMMAND ln -s fatrat ${DESTDIR}/bin/fatrat-nogui)
-else (DESTDIR)
-	EXECUTE_PROCESS(COMMAND ln -s fatrat ${CMAKE_INSTALL_PREFIX}/bin/fatrat-nogui)
-endif (DESTDIR)
-
+EXECUTE_PROCESS(COMMAND ln -s fatrat "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/fatrat-nogui")
