e88fdbea245472702a07c18c771f5cacd0a5a33d
Lubos Dolezel <lubos@dolezel.info>
2011-11-06 21:36:26 +0100

    FatRat 1.2.0_beta1

--- src/fatrat.h
+++ src/fatrat.h
@@ -38,7 +38,7 @@ respects for all of the code used other than "OpenSSL".
 #include <QNetworkProxy>
 #include "DelayedIcon.h"
 
-#define VERSION "1.2.0-beta1"
+#define VERSION "1.2.0_beta1"
 
 QString formatSize(qulonglong size, bool persec = false);
 QString formatTime(qulonglong secs);
