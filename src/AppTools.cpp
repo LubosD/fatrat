f6fb6a73e9560bb4dbb117f42577a66a8aa61589
Lubos Dolezel <lubos@dolezel.info>
2012-01-29 16:03:56 +0100

    Remove RapidTools (defunct and will be superseded)

--- src/AppTools.cpp
+++ src/AppTools.cpp
@@ -38,7 +38,6 @@ respects for all of the code used other than "OpenSSL".
 #	include "tools/FileSharingSearch.h"
 #endif
 
-#include "tools/RapidTools.h"
 #include "tools/VideoFetcher.h"
 #include "tools/HashDlg.h"
 
@@ -50,8 +49,6 @@ void initAppTools()
 	g_tools << AppTool(QObject::tr("Torrent search"), TorrentSearch::create);
 	g_tools << AppTool(QObject::tr("Create a torrent"), CreateTorrentDlg::create);
 #endif
-	g_tools << AppTool(QObject::tr("RapidShare tools"), RapidTools::create);
-	//g_tools << AppTool(QObject::tr("Video fetcher"), VideoFetcher::create);
 	g_tools << AppTool(QObject::tr("File hasher"), HashDlg::create);
 #ifdef WITH_JPLUGINS
 	g_tools << AppTool(QObject::tr("File sharing server search"), FileSharingSearch::create);
